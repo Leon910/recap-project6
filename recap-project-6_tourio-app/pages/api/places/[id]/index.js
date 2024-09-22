a5562961b8e3c718331c0d79198288dfbd034fa5
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-22 11:54:35 +0200

    add update logic

--- recap-project-6_tourio-app/pages/api/places/[id]/index.js
+++ recap-project-6_tourio-app/pages/api/places/[id]/index.js
@@ -9,27 +9,16 @@ export default async function handler(request, response) {
     return;
   }
 
-  try {
-    if (request.method === "GET") {
-      const place = await Place.findById(id);
-      response.status(200).json(place);
-      return;
-    }
-
-    if (request.method === "PATCH") {
-      const place = await Place.findByIdAndUpdate(id);
-      response.status(200).json(place);
-      return;
-    }
-  } catch (error) {
-    return response.status(400).json({ error: "Places not found" });
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+    response.status(200).json(place);
+    return;
   }
 
-  // const place = places.find((place) => place.id === id);
-
-  // if (!places) {
-  //   return response.status(404).json({ status: "Not found" });
-  // }
-
-  // response.status(200).json(places);
+  if (request.method === "PATCH") {
+    const placeToUpdate = await Place.findByIdAndUpdate(id, {
+      $set: request.body,
+    });
+    response.status(200).json(placeToUpdate);
+  }
 }
--- recap-project-6_tourio-app/pages/places/[id]/index.js
+++ recap-project-6_tourio-app/pages/places/[id]/index.js
@@ -31,11 +31,11 @@ const StyledLocationLink = styled(StyledLink)`
 export default function DetailsPage() {
   const router = useRouter();
   const { isReady } = router;
-  const { frontId } = router.query;
+  const { id } = router.query;
 
-  const { data, isLoading, error } = useSWR(`/api/places/${frontId}`);
+  const { data, isLoading, error } = useSWR(`/api/places/${id}`);
 
-  console.log(frontId);
+  console.log(id);
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
 
@@ -67,7 +67,7 @@ export default function DetailsPage() {
       </Link>
       <p>{data.description}</p>
       <ButtonContainer>
-        <Link href={`/places/${frontId}/edit`} passHref legacyBehavior>
+        <Link href={`/places/${id}/edit`} passHref legacyBehavior>
           <StyledLink>Edit</StyledLink>
         </Link>
         <StyledButton onClick={deletePlace} type="button" $variant="delete">
